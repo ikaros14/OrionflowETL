00c483db0fec641c4d63dd85f97cf0e38bb5f6a5
Absalon Hernandez <me@absa.gg>
2026-04-06 17:56:45 -0600

    feat: add C# 11 required member polyfills for pre-NET7 targets

--- OrionflowETL.Adapters.SqlServer/Polyfills.cs
+++ OrionflowETL.Adapters.SqlServer/Polyfills.cs
@@ -1,15 +1,16 @@
 #if !NET7_0_OR_GREATER
-namespace System.Runtime.CompilerServices;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-internal sealed class RequiredMemberAttribute : Attribute { }
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-internal sealed class CompilerFeatureRequiredAttribute : Attribute
+namespace System.Runtime.CompilerServices
 {
-    public string FeatureName { get; }
-    public bool IsOptional { get; init; }
-    public CompilerFeatureRequiredAttribute(string featureName) { FeatureName = featureName; }
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class RequiredMemberAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public string FeatureName { get; }
+        public bool IsOptional { get; init; }
+        public CompilerFeatureRequiredAttribute(string featureName) { FeatureName = featureName; }
+    }
 }
 
 namespace System.Diagnostics.CodeAnalysis
