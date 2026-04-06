4e0d8751e5d69e42d9493df4231f968530b4077d
Absalon Hernandez <me@absa.gg>
2026-04-06 17:53:46 -0600

    feat: add multi-target support, polyfills for older frameworks, and new adapter projects for Postgres, MySQL, SQL Server, and CSV.

---
+++ OrionflowETL.Adapters.MySql/Polyfills.cs
@@ -0,0 +1,20 @@
+#if !NET7_0_OR_GREATER
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+internal sealed class RequiredMemberAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+internal sealed class CompilerFeatureRequiredAttribute : Attribute
+{
+    public string FeatureName { get; }
+    public bool IsOptional { get; init; }
+    public CompilerFeatureRequiredAttribute(string featureName) { FeatureName = featureName; }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    internal sealed class SetsRequiredMembersAttribute : Attribute { }
+}
+#endif
---
+++ OrionflowETL.Core/Polyfills.cs
@@ -0,0 +1,22 @@
+#if !NET7_0_OR_GREATER
+namespace System.Runtime.CompilerServices;
+
+using System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+internal sealed class RequiredMemberAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+internal sealed class CompilerFeatureRequiredAttribute : Attribute
+{
+    public string FeatureName { get; }
+    public bool IsOptional { get; init; }
+    public CompilerFeatureRequiredAttribute(string featureName) { FeatureName = featureName; }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    internal sealed class SetsRequiredMembersAttribute : Attribute { }
+}
+#endif
